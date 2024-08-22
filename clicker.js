37426de1cf6bcc8c53aff5b9a29fbbbc78f980c2
Your Name <your-email@example.com>
2024-08-22 05:44:17 +0300

    аше сообщение о коммите

--- clicker.js
+++ clicker.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const clickerImage = document.getElementById('clicker-image');
     const energyCountElement = document.getElementById('energy-count');
-    const boostButton = document.getElementById('boost-button'); // Кнопка Boost
+    const boostButton = document.querySelector('.boost-button'); // Кнопка Boost
+    const boostStatus = document.getElementById('boost-status'); // Статусный значок на кнопке Boost
 
     let coinCount = parseInt(localStorage.getItem('coins')) || 0;
     let energyCount = parseInt(localStorage.getItem('energy')) || 100;
@@ -70,7 +71,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Обработчик для кнопки Boost
     if (boostButton) {
         boostButton.addEventListener('click', (event) => {
+            // Проверяем, есть ли достаточно энергии для перехода
             if (energyCount > 0) {
+                // Удаляем значок на кнопке Boost
+                if (boostStatus) {
+                    boostStatus.style.display = 'none';
+                }
+
                 window.location.href = 'puzzle.html'; // Перенаправляем на страницу с игрой
             } else {
                 alert('Not enough energy to use boost!');
